7f72f931eaeeeda71091c5ca0cb439c14b79ab34
KJS <digyrh456789@gmail.com>
2022-08-01 14:24:14 +0900

    wow

--- sette/src/List.jsx
+++ sette/src/List.jsx
@@ -1,17 +1,31 @@
 import React, {useState} from "react";
 import { ReactComponent as Bar } from "./Line 2.svg";
 import './List.css'
+import Texting from "./Texting";
 export default function List() {
+    //날짜 구하기 위해 필요한 변수
     let now = new Date();
+    //요일
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+    let dayOfWeek = week[now.getDay()];
+    //월
     let Month = now.getMonth() + 1;
     if(Month < 10){
         Month = `0${Month}`;
     }
+    //일
     let Day = now.getDate();
+    if(Day < 10){
+        Day = `0${Day}`;
+    }
+
+    
+
     return(
         <div>
             <Bar className='bars'/>
-            <p>{Month}</p>
+            <p className="today">{Month} / {Day} ({dayOfWeek})</p>
+            <Texting/>
         </div>
     )
 }
---
+++ sette/src/Texting.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+export default function Texting() {
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const onReset = () => {
+        setText('');
+    };
+
+    return (
+        <div className='texting'>
+            <input className='inputing' onChange={onChange} value={text}  />
+            <button onClick={onReset}>초기화</button>
+        </div>
+    );
+}
