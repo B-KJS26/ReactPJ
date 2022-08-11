9a433c93bf63f1fb28564b3779bfbf2e722ddbdd
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-08-11 23:50:06 +0900

    Update Texting.jsx

--- sette/src/Texting.jsx
+++ sette/src/Texting.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 
 export default function Texting() {
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const onReset = () => {
+        setText('');
+    };
 
     return (
         <div className='texting'>
-            <input className='inputing' value={text}  />
-            <button onClick={onReset}>초기화</button>
+            <input className='inputing' onChange={onChange} value={text}  />
         </div>
     );
 }
